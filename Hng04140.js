edc631509cc4d5623df0794f710731ee264fe53d
CodzSchach <40149639+Joker4mas@users.noreply.github.com>
2020-06-04 03:14:09 -0700

    Add files via upload
    The file was edited to display my information using JavaScript

---
+++ Hng04140.js
@@ -0,0 +1,24 @@
+function Hng_task2() {
+  const name = "Samuel Ogbaje";
+  const id = "HNG-04140";
+  const language = "JavaScript";
+  const Email = "Samuelogbaje@gmail.com.";
+  console.log(
+    "Hello World, this is" +
+      " " +
+      name +
+      " " +
+      "with HNGi7_ID:" +
+      id +
+      " " +
+      "using" +
+      " " +
+      language +
+      " " +
+      "for stage 2 task." +
+      " " +
+      "email:" +
+      Email
+  );
+}
+Hng_task2();
